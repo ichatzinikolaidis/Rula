0d74c751544e334e4c6a1b5cbdd870c29eac09b5
ichatzinikolaidis <lohatzis@gmail.com>
2016-03-15 14:14:40 +0200

    catkin setup

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/iordanis/Roula/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/iordanis/LearningROS/devel;/opt/ros/indigo".split(';') if "/home/iordanis/LearningROS/devel;/opt/ros/indigo" != "" else []
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/iordanis/Roula/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/iordanis/Roula/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/iordanis/Roula/devel/lib:/home/iordanis/Roula/devel/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
+export PATH="/home/iordanis/Roula/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/iordanis/Roula/devel/lib/pkgconfig:/home/iordanis/Roula/devel/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/iordanis/Roula/build"
+export PYTHONPATH="/home/iordanis/Roula/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/iordanis/Roula/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/iordanis/Roula/src:$ROS_PACKAGE_PATH"
