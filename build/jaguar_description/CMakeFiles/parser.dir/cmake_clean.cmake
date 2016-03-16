dd5b3f7412a071b62aa4c5877c670a02dd1825ed
ichatzinikolaidis <lohatzis@gmail.com>
2016-03-16 15:17:36 +0200

    Update Roula's description

---
+++ build/jaguar_description/CMakeFiles/parser.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/parser.dir/src/parser.cpp.o"
+  "/home/iordanis/Roula/devel/lib/jaguar_description/parser.pdb"
+  "/home/iordanis/Roula/devel/lib/jaguar_description/parser"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/parser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
